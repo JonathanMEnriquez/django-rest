9e6228989bc83bc883c993cf826344c9c76fd572
user <user@users-MacBook-Pro.local>
2018-06-25 20:05:38 -0700

    completed user superuser and usermanager in models

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+test = [1,2,3,4,5,6,7,8,85,22,13,14]
+
+filtered = [test[i] for i in range(len(test)) if test[i] % 2 == 0]
+
+print(filtered)
--- src/profiles_project/apps/profiles_api/models.py
+++ src/profiles_project/apps/profiles_api/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Methods for our custom user model"""
+
+    def create_user(self, email, name, password="None"):
+        if not email:
+            raise ValueError('Please provide an email address.')
+        email = self.normalize_email(email)
+        user = self.model(name = name, email = email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_super_user(self, email, name, password):
+        """Creates and saves a new super user"""
+
+        user = self.create_user(email, name, password)
+        user.is_staff = True
+        user.is_super_user = True
+
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Repesent a User Profile inside our system"""
+
+    email = models.EmailField(max_length = 255, unique = True)
+    name = models.CharField(max_length = 255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+    is_super_user = models.BooleanField(default = False)
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Use to get a user's full name"""
+
+        return self.name
+
+    def get_short_name(self):
+        """Use to get a user's short name"""
+
+        return self.name
+
+    def __str__(self):
+        return self.email
