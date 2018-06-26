40a3226f8bcce12d6ebc013abd877e19f53d0f5d
user <user@users-MacBook-Pro.local>
2018-06-26 14:35:58 -0700

    added own profile permission

--- src/profiles_project/apps/profiles_api/views.py
+++ src/profiles_project/apps/profiles_api/views.py
@@ -3,9 +3,11 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 from . import serializers
 from . import models
+from . import permissions
 
 # Create your views here.
 
@@ -103,7 +105,9 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({'method', 'delete'})
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """"""
+    """Handles CRUD for the user profiles"""
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
