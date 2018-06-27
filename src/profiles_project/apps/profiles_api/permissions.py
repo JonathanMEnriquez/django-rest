2077e7fcc97bad0284aac531de94d1dc2dbb8953
user <user@users-MacBook-Pro.local>
2018-06-26 17:05:05 -0700

    completed api

--- src/profiles_project/apps/profiles_api/permissions.py
+++ src/profiles_project/apps/profiles_api/permissions.py
@@ -12,3 +12,15 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+class PostOwnStatus(permissions.BasePermission):
+    """check if user is logged in before allowing to create new status update"""
+
+    def has_object_permission(self, request, view, obj):
+        """checks if the user is trying to update their status"""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_profile.id == request. user.id
+
